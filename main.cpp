9a3e2c9f54c3f2f10fe38a775758985881126a14
GulTion <>
2021-04-02 15:51:54 +0000

    at the END ; things are Working

--- main.cpp
+++ main.cpp
@@ -1,136 +1,209 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 
-struct login_signup
-{
-    char username[200];
-    char password[200];
-}userdata;
+int const MAX_CHAR  =30;
+int const II = sizeof(int);
+int const CC = sizeof(char);
 
-FILE *data = NULL;
-int setup(int situation);
-void login();
-int find_occ(char* string, char to_find);
-void create_account();
-int find_occ_t(char* string, char to_find, int times, int indexes[]);
+struct user{
+  char name[MAX_CHAR];
+  char pass[MAX_CHAR];
+}you;
 
 
-int main(){
-    int choice;
-    printf("What you wanna do?\n");
-    printf("1. Login\n");
-    printf("2. Register\n");
-    printf("> ");
-    scanf("%d", &choice);
-    if (choice==1){
-        login();
-    }
-    else if (choice==2)
-    {
-        create_account();
-        // system("cls");
-        system("clear"); // only for the Linux 
-        printf("Your account has been created.\nLogin to continue.\n");
-        login();
-    }
-    else{
-        printf("Wrong choice dude.");
-    }
+struct info{
+  int how_many=0;
+  int user_index=0;
+}db_info;
 
+
+void countUser(FILE *db){
+  int c=0;
+  fread(&db_info.how_many, II, 1,db);
+}
+
+bool checkPass(){
+
+  FILE * f = fopen("db.db", "r");
+  countUser(f);
+  struct user u;
+  int how = db_info.user_index;
+  fseek(f, II, SEEK_SET);
+  while(how--){
+  fread(&u.name, CC, MAX_CHAR, f);
+  fread(&u.pass, CC, MAX_CHAR, f);
+    if(strcmp(u.pass, you.pass)==0){
+
+      return true;
+
+    
+  }
+
+  }
+
+  fclose(f);
+  return false;
+
+}
+
+bool checkUser(){
+  // if is_pass is true then it will match for password
+  // if is_pass is false then it will match for user
+
+  bool isExist = false;
+  FILE * f = fopen("db.db", "r");
+  countUser(f);
+  struct user u;
+  int how = db_info.how_many;
+  fseek(f, II, SEEK_SET);
+  while(how--){
+  fread(&u.name, CC, MAX_CHAR, f);
+  fread(&u.pass, CC, MAX_CHAR, f);
+  
+    if(strcmp(u.name, you.name)==0){
+      db_info.user_index = db_info.how_many-how;
+      return true;
+
+    
+  }
+
+  }
+
+  fclose(f);
+   return false;
    
+};
+
+
+
+
+
+void addUser(){
+  if(checkUser()){
+    printf("User `%s` is Already Exist!\n", you.name);
+    exit(0);
+  }
+
+  FILE * f = fopen("db.db", "a+");
+  fwrite(you.name, CC, MAX_CHAR, f);
+  fwrite(you.pass, CC, MAX_CHAR, f);
+  fclose(f);
+
+  printf("Hurry ! `%s` You Account is Created !\n",you.name);
+  
+  f = fopen("db.db", "r+");
+  int j= ++db_info.how_many;
+  fwrite(&j, II, 1, f);
+  fclose(f);
+
+
+}
+
+void viewAllUser(){
+  struct user u;
+  
+  
+  FILE * f = fopen("db.db", "r");
+  countUser(f);
+  int how = db_info.how_many;
+  fseek(f, II, SEEK_SET);
+
+  while(how--){
+  fread(&u.name, CC, MAX_CHAR, f);
+  fread(&u.pass, CC, MAX_CHAR, f);
+  printf("%s:%s\n",u.name,u.pass);
+  }
+  fclose(f);
+  
+}
+
+void UserWelcome(){
+  
+  printf("`%s` is SuccessFully Login !\n",you.name);
+}
+
+void loginUser(){
+  if(!checkUser()){
+    printf("`%s` not find in Our Database! 404!\n",you.name);
+  exit(0);
+   
+  }if(!checkPass()){
+     printf("Password is inCorrent for `%s`!\n",you.name);
+    exit(0);
+  }
     
-    return 0;
+   
+
+  UserWelcome();
+
 }
 
-void create_account(void){
-    printf("Choose a username for your account: ");
-    scanf("%s", userdata.username);
-    printf("Choose a password for your account: ");
-    scanf("%s", userdata.password);
-    setup(1);
+void Screen(bool type){
+  // type = true|false // true:login | false:signup
+
+
+  // Clear the the Screen;
+  system("clear");
+  printf("Enter UserName: ");scanf("%s", you.name);
+  printf("Enter Password: ");scanf("%s", you.pass);
+
+  // printf("%s:%s", you.name,you.pass);
+  type?loginUser():addUser();
+
 }
 
-void login(void){
-    char temp[200];
-    printf("Username: ");
-    scanf("%s", temp);
-    if (strcmp(temp,userdata.username) !=0){
-            printf("Wrong dude");
-            exit(0);
-        }
-        printf("Password: ");
-        scanf("%s", temp);
-        if (strcmp(temp,userdata.password) !=0){
-            printf("Wrong dude");
-            exit(0);
-        }
-        setup(2);
-}
-
-int setup(int situation){
-    if (situation==1){
-        char info;
-        data = fopen("data.txt", "a");
-        if (data==NULL){
-            printf("The file you specified doesn't exits!");
-            exit(0);
-        }
-        strcpy(&info, userdata.username);
-        strcat(&info, ": ");
-        strcat(&info, userdata.password);
-        fputs(&info, data);
-        fputs("\n", data);
-        fclose(data);
-    }
-    else if (situation==2)
-    {
-        // login
-        char usernames[200];
-        char passwords[200];
-        char c;
-        char info[200];
-        int occurrences, indexes[20000];
-        data = fopen("data.txt", "r");
-        if (data==NULL){
-            data = fopen("data.txt", "a");
-        }
-        c = fgetc(data);
-        int length=0;
-        while (c!=EOF)
-        {
-            strncat(info, &c, 1);
-            c = fgetc(data);
-            length++;
-        }
-        info[length]=0;
-        // find the occurrences of : then run loop that times and then seperate usernames and passwords
-        occurrences = find_occ(info, ':');
-        find_occ_t(info, ':', occurrences, indexes);
-        printf("%d", indexes[0]);   
+void checkForDB(){
+    FILE *f = fopen("db.db","r");
+    int i=0;
+    if(f==NULL){
+       printf("`db.db` Not Found! run command `./%s --init-db`\n","main");
+       
+       exit(0);
+       fclose(f);
     }
     
-    return 0;
+    
+    
+}
+
+void welcomeScreen(){
+  int choice;
+  // Choice's Options
+  // 1 for Login
+  // 2 for Register
+  printf("Welcome !!\n");
+  printf("1. For Login\n2. For Register\n");
+  printf("Enter Your Choice: ");
+  scanf("%d", &choice);
+
+  switch(choice){
+    case 1: Screen(true);
+    break;
+    case 2: Screen(false);
+    break;
+  }
+}
+
+void createNewDB(int argc, char *argv[]){
+  if(argc==2)
+    if(strcmp(argv[1], "--init-db")==0){
+      FILE *f = fopen("db.db","w");
+      int i=0;
+    fwrite(&i, II, 1,f);
+    fclose(f);
+    exit(0);
+    }
 }
 
 
-int find_occ(char* string, char to_find){
-    int occurrences=0;
-    for (int i=0; i<strlen(string); i++){
-        if (string[i]==to_find){
-            occurrences++;
-        }
-    }
-    return occurrences;
-}
 
-int find_occ_t(char* string, char to_find, int times, int indexes[]){
-    int count = 0;
-    for (int i=0; count<times; i++){
-        if (string[i]==to_find){
-            indexes[count]=i;
-            count++;
-        }
-    }
-    return 0;
+int main(int argc, char *argv[]){
+  
+  createNewDB(argc, argv);
+  checkForDB();
+  welcomeScreen();
+  // viewAllUser(); // uncomment for view all user list
+  // printf("%d", db_info.how_many);
+  
 }
